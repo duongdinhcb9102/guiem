2a7dc7424e0771e906a98059e50df247b46ad10b
duongdinhcb9102 <89021392+duongdinhcb9102@users.noreply.github.com>
2021-08-17 01:32:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,11 +4,11 @@ const CONFIG = {
     vay mà em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'úihihi🤭',
     title: 'Phải chăng anh đã cảm nắng từ lần đầu gặp em ',
     desc: 'Phải chăng anh đã cảm nắng từ lần đầu gặp em ',
     btnYes: 'Vậy yêu anh nhóa em <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnNo: 'Không, Anh à em chỉ coi anh là bạn thôi:3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Nói cho anh đi baby <3',
     reply: 'Yêu thì yêu mà không yêu thì vẫn phải yêu <3',
