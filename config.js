178aaa1cd010a199156a36b5881bcc8ac457fe9d
duongdinhcb9102 <89021392+duongdinhcb9102@users.noreply.github.com>
2021-08-16 20:19:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
+    introTitle: 'Em à<3!',
+    introDesc: `Trái đất vốn đã rất lạ thường
+    Vậy sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    btnIntro: 'hihi🙈',
+    title: 'Phải chăng anh đã bị say nắng từ cái nhìn đầu tiên về em<3',
+    desc: 'Phải chăng anh đã bị say nắng từ cái nhìn đầu tiên về em< ',
+    btnYes: 'Vậy yêu anh nhóa <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' /duongdinhnehi11/
 }
