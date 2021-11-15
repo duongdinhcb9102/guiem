e73650e87bd76e054a97f176f4eddd409846880b
duongdinhcb9102 <89021392+duongdinhcb9102@users.noreply.github.com>
2021-11-15 21:38:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Em à!',
-    introDesc: `Trái đất vốn dã lạ thường
-    vay mà em cứ đi nhầm đường
+    introTitle: 'Thủy à!',
+    introDesc: `Trái đất vốn rất dã lạ thường
+    vay mà thủy cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Đằng sau chữ yêu đây là chữ thương`,
     btnIntro: 'úihihi🤭',
-    title: 'Phải chăng anh đã cảm nắng từ lần đầu gặp em ',
-    desc: 'Phải chăng anh đã cảm nắng từ lần đầu gặp em ',
-    btnYes: 'Vậy em đồng ý yêu anh nhóa em <33',
-    btnNo: 'Không,anh ạ:3',
-    question:'wow vậy là em đồng ý rồi à, hãy nói lý do sao em chọn đju<3',
-    btnReply: 'Nói cho anh đi baby <3',
-    reply: 'Yêu thì yêu mà không yêu thì vẫn phải yêu <3',
-    mess: 'Wow anh biết ngay mà 🥰. Yêu em nhiều lắm😘',
-    messDesc: 'Tối nay 8h anh qua đón nhé công chúa.',
+    title: 'Phải chăng tớđã cảm nắng từ lần đầu thấy thủy cười',
+    desc: 'Phải chăng tớ đã cảm nắng từ lần đầu thấy thủy cười ',
+    btnYes: 'Vậy thủy đồng ý làm mảnh đời của tớ nhóa <33',
+    btnNo: 'Không, ạ:3',
+    question:'wow vậy là thủy đồng ý rồi đó , hãy nói lý do sao thủy chọn đju<3',
+    btnReply: 'Nói cho tớ đi honey <3',
+    reply: 'Yêu thì yêu mà không yêu thì vẫn phải yêu đó <3',
+    mess: 'Wow tớ biết ngay mà 🥰. Yêu tớ nhiều lắm😘',
+    messDesc: 'khi nào đi học tớ qua đón thủy đi tà tưa nhoá .',
     btnAccept: 'Oki lun <3',
     messLink: 'http://fb.com/duongdinhnehi11', 
 }
